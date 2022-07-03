9de08ca3f0afede0ca65bc063476ac3c7135f1fd
Evgeny Zbirun <zbirunzbirun@gmail.com>
2022-07-02 23:56:01 -0700

    function generateArray makes array from 0 to input number and saves it.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 function generateArray(input) {
   const inputArray = [];
   for (let index = 0; index <= input; index += 1) {
-    inputArray.push(index.)
+    inputArray.push(index);
   }
+  return inputArray;
 }
