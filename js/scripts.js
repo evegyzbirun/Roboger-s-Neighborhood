4d32f20fe4f71ea48ac497da33c56a4fcce487a3
Evgeny Zbirun <zbirunzbirun@gmail.com>
2022-07-03 00:52:48 -0700

    made an array of three words and steps for building function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,15 @@
+const inputArray = [];
 function generateArray(input) {
-  const inputArray = [];
+
   for (let index = 0; index <= input; index += 1) {
     inputArray.push(index);
   }
   return inputArray;
 }
 
-let newArray = []
-newArray.push(inputArray[index].toString());
-
+let words = ["Beep!", "Boop!", "Won't you be my neighbor?"];
+// words = 0, 1, 2 elements
+// 0  "Beep!" will be used if we have 1 in number from inpuArray and no 2, 3
+// 1  "Boop!" will be used if we have 2 in number from inpuArray and no 3
+// 2 "Won't you be my neighbor?" will be used if we have 3 in number from inpuArray (1 and 2 does't effect the result)
+// if inputArray has 
