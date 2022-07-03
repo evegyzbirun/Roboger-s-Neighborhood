be050aed4692d79272e29d355f58e5c0357446ea
Evgeny Zbirun <zbirunzbirun@gmail.com>
2022-07-03 06:56:29 -0700

    keep working.

--- js/scripts.js
+++ js/scripts.js
@@ -12,4 +12,5 @@ let words = ["Beep!", "Boop!", "Won't you be my neighbor?"];
 // 0  "Beep!" will be used if we have 1 in number from inpuArray and no 2, 3
 // 1  "Boop!" will be used if we have 2 in number from inpuArray and no 3
 // 2 "Won't you be my neighbor?" will be used if we have 3 in number from inpuArray (1 and 2 does't effect the result)
-// if inputArray has 
+// if inputArray has
+//
