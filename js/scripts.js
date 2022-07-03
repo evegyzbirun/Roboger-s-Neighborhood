f666be5c3de9cca5fea21db4a0f9fb918b232750
Evgeny Zbirun <zbirunzbirun@gmail.com>
2022-07-03 08:04:30 -0700

    using forEach to work with new array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
-const inputArray = [];
+// words = 0, 1, 2 elements
+// 0  "Beep!" will be used if we have 1 in number from inpuArray and no 2, 3
+// 1  "Boop!" will be used if we have 2 in number from inpuArray and no 3
+// 2 "Won't you be my neighbor?" will be used if we have 3 in number from inpuArray (1 and 2 does't effect the result)
+// if inputArray has
+//
+let inputArray = [];
 function generateArray(input) {
 
   for (let index = 0; index <= input; index += 1) {
@@ -8,15 +14,18 @@ function generateArray(input) {
 }
 
 let words = ["Beep!", "Boop!", "Won't you be my neighbor?"];
-// words = 0, 1, 2 elements
-// 0  "Beep!" will be used if we have 1 in number from inpuArray and no 2, 3
-// 1  "Boop!" will be used if we have 2 in number from inpuArray and no 3
-// 2 "Won't you be my neighbor?" will be used if we have 3 in number from inpuArray (1 and 2 does't effect the result)
-// if inputArray has
-//
+
 let finalArray = [];
 inputArray.forEach(function (element) {
-  if (element.includes("3")) {
-    element.replace
+  if (element.includes(3)) {
+    inputArray[index] = "Won't you be my neighbor?"
+    // or I need to find how to replace on words[2] "Won't you be my neighbor?"
+  } else if (element.includes(2)) {
+    inputArray[index] = "Boop!"
+  } else if (element.includes(1)) {
+    inputArray[index] = "Beep!"
+  } else {
+
   }
-})
+  return inputArray;
+});
