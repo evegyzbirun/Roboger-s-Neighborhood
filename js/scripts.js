04595547917f0a33850b7317493a485736401f9b
Evgeny Zbirun <zbirunzbirun@gmail.com>
2022-07-10 08:04:51 -0700

    functions work with Dom.

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,33 @@
-// words = 0, 1, 2 elements
-// 0  "Beep!" will be used if we have 1 in number from inpuArray and no 2, 3
-// 1  "Boop!" will be used if we have 2 in number from inpuArray and no 3
-// 2 "Won't you be my neighbor?" will be used if we have 3 in number from inpuArray (1 and 2 does't effect the result)
-// if inputArray has
-//
 
+let inputArray = [];
 function generateArray(input) {
-
-  let inputArray = [];
-
   for (let index = 0; index <= input; index += 1) {
-    inputArray.push(index);
-
+    inputArray.push(index.toString());
   }
-  let newArray = inputArray.toString().split(',');
-
-  return newArray;
-
 }
 
-//once you return you will have something like this [1,2,3,4]
 
-//another helper function here youFunct(array) that will loop through and 
 
 
 
 function outPut() {
-  let input2 = generateArray();
+
   let finalArray = [];
-  input2.forEach(function (element) {
+  inputArray.forEach(function (element) {
 
     if (element.includes("3")) {
-      // string.replace("Won't you be my neighbor?");
-      finalArray.push(element);
+
+      finalArray.push("Won't you be my neighbor?");
     } else if (element.includes("2")) {
-      // string.replace("Boop!");
-      finalArray.push(element);
+
+      finalArray.push("Boop!");
     } else if (element.includes("1")) {
-      // string.replace("Beep!");
-      finalArray.push(element);
+
+      finalArray.push("Beep!");
     } else {
       finalArray.push(element);
     }
-    //this will look like [0, beep, boop, wybmn, 4,5,6....]
+
   });
   return finalArray;
 }
@@ -53,16 +38,11 @@ function twoInOne(event) {
   event.preventDefault();
   const inputNumber = document.getElementById("userNumber").value;
   const arrayFromNumber = generateArray(inputNumber);
-  const outputLine = outPut(arrayFromNumber);
-  document.querySelector("div#output").append(arrayFromNumber);
+  const outputLine = outPut();
+  document.querySelector("div#output").append(outputLine);
 }
 
 window.addEventListener("load", function () {
   document.getElementById("replace").addEventListener("submit", twoInOne);
 
-})
-// when you do your page load and submit
-//get value from form input= input.value
-// let inputArray = generateArray(input);
-//let finalarray= yourfunction(inputArray);
-// post and clear results to DOM
+});
